5b7948e1cdb4196d3dd4dc91dba1a4db8a37be03
ei-show <ei.show@gmail.com>
2021-02-28 07:18:08 +0900

    fontAwesomeを使うための準備

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,31 @@
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import {
+    faHeadset,
+    faEdit,
+    faCode,
+    faBriefcase,
+    faUser,
+    faBlog,
+    faEnvelope,
+    faWindowMaximize,
+    faWifi,
+} from '@fortawesome/free-solid-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
+library.add(
+    faHeadset,
+    faEdit,
+    faCode,
+    faBriefcase,
+    faUser,
+    faBlog,
+    faEnvelope,
+    faWindowMaximize,
+    faWifi,
+    faYoutube,
+);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   return (
     <>
-    <h1>lean fontAwesome</h1>
+      <h1>lean fontAwesome</h1>
+      <FontAwesomeIcon icon="window-maximize" />
+      <FontAwesomeIcon icon="wifi" size="2x"/>
+      <FontAwesomeIcon icon={['fab', 'youtube']} size="2x" fixedWidth />
+      <FontAwesomeIcon className="" icon="headset" size="2x" fixedWidth />
+
     </>
   )
 }
